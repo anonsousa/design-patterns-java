5a98b6f8c5a6da646c303314da14f067be93bea5
Antonio <pisouza011@gmail.com>
2024-05-20 09:12:07 -0300

    Implementing View (MVC)

--- src/main/java/model/Pis.java
+++ src/main/java/model/Pis.java
@@ -1,3 +1,5 @@
+package model;
+
 import java.util.Observable;
 
 public class Pis extends Observable implements Imposto{
---
+++ src/main/java/view/TelaPis.java
@@ -0,0 +1,50 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.Observable;
+import java.util.Observer;
+
+public class TelaPis implements TelaImposto, Observer {
+
+    private TextField txtValue;
+    private Button btnCalc;
+
+    public TelaPis() {
+        Frame frame = new Frame("Calc Pis Tax");
+        frame.add("North", new Label("Value to calc"));
+        txtValue = new TextField();
+        frame.add("Center", txtValue);
+
+        Panel panel = new Panel();
+        btnCalc = new Button("Calc");
+        panel.add(btnCalc);
+        frame.add("South", panel);
+
+        frame.addWindowListener(new CloseListener());
+        frame.setSize(200, 150);
+        frame.setLocation(100, 100);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public float getValue() {
+        return Float.parseFloat(txtValue.getText());
+    }
+
+    @Override
+    public void update(Observable model, Object state) {
+        String msg = model.getClass() + " " + state;
+        JOptionPane.showMessageDialog(null, msg);
+    }
+
+    public static  class CloseListener extends WindowAdapter{
+        public void windowClosing(WindowEvent event){
+            event.getWindow().setVisible(false);
+            System.exit(0);
+        }
+    }
+
+}
